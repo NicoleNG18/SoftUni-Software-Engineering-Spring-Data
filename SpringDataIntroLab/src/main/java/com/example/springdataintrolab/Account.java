0745c5600656d839fa3cd43a12e4de3dab2d39c9
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-11-27 21:24:02 +0200

    Updating pom.xml and adding Account class

--- SpringDataIntroLab/src/main/java/com/example/springdataintrolab/Account.java
+++ SpringDataIntroLab/src/main/java/com/example/springdataintrolab/Account.java
@@ -1,4 +1,35 @@
 package com.example.springdataintrolab;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+@Entity
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private BigDecimal balance;
+
+    public Account(){
+        this.balance=BigDecimal.ZERO;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
