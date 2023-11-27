0a70e3d7b4cd2bcad4e0587a4b81060cdbe599ca
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-11-27 21:24:49 +0200

    Adding user class

--- SpringDataIntroLab/src/main/java/com/example/springdataintrolab/User.java
+++ SpringDataIntroLab/src/main/java/com/example/springdataintrolab/User.java
@@ -1,4 +1,58 @@
 package com.example.springdataintrolab;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private Integer age;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Account> accounts;
+
+    public User() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public User(String username, int age, Account account) {
+        this();
+
+        this.username = username;
+        this.age = age;
+        this.accounts.add(account);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
