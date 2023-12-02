8fbf8de1e7c8da39540d32865a0b10b53ff95ec6
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-12-02 09:58:34 +0200

    Db access exercise

---
+++ DBAccessExercise/src/main/java/ColumnLabels.java
@@ -0,0 +1,9 @@
+public enum ColumnLabels {
+    ;
+    static final String COLUMN_LABEL_NAME = "name";
+    static final String COLUMN_LABEL_COUNT = "count";
+    static final String COLUMN_LABEL_AGE = "age";
+
+    static final String COLUMN_LABEL_ID = "id";
+
+}
