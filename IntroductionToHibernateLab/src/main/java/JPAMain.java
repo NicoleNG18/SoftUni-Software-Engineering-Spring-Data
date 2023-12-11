108a75b9d714afbf93037eb96a776913c3e6ec14
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-12-11 21:01:30 +0200

    Intro to Hibernate lab

---
+++ IntroductionToHibernateLab/src/main/java/JPAMain.java
@@ -0,0 +1,23 @@
+import entities.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory=
+                Persistence.createEntityManagerFactory("school-db");
+
+        EntityManager entityManager= entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        Student student=new Student("nikol");
+        entityManager.persist(student);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
+
